97c83bdbe7945b7fb2ad4dd20c97e723fddd9bd4
Igor Karolesky <igorfk96@gmail.com>
2020-02-28 17:03:59 -0300

    removido placeholder plataformas em adicionar produtos

--- resources/views/products/_forms2.blade.php
+++ resources/views/products/_forms2.blade.php
@@ -28,7 +28,7 @@
     $("#add").click(function(){
         if (i >= 2) return;
         ++i;
-        $("#dynamicTable").append('<tr><td><select id="plataforma" name="addmore['+i+'][plataforma]" placeholder="Plataforma" class="form-control">@foreach($plataforma as $plat)<option value="{{$plat->id}}">{{$plat->name}}</option>@endforeach</select></td><td><input type="text" name="addmore['+i+'][codigo_produto]" placeholder="Código do Produto" class="form-control" /></td><td><input type="text" name="addmore['+i+'][product_key]" placeholder="Product Key" class="form-control" /></td><td><input type="text" name="addmore['+i+'][basic_authentication]" placeholder="Basic Authentication" class="form-control" /></td><td><button type="button" class="btn btn-icon btn-icon rounded-circle btn-danger mr-1 mb-1 remove-tr"><i class="feather icon-minus"></i></button></td></tr>');
+        $("#dynamicTable").append('<tr><td><select id="plataforma" name="addmore['+i+'][plataforma]" class="form-control">@foreach($plataforma as $plat)<option value="{{$plat->id}}">{{$plat->name}}</option>@endforeach</select></td><td><input type="text" name="addmore['+i+'][codigo_produto]" class="form-control" /></td><td><input type="text" name="addmore['+i+'][product_key]" class="form-control" /></td><td><input type="text" name="addmore['+i+'][basic_authentication]" class="form-control" /></td><td><button type="button" class="btn btn-icon btn-icon rounded-circle btn-danger mr-1 mb-1 remove-tr"><i class="feather icon-minus"></i></button></td></tr>');
     });
    $(document).on('click', '.remove-tr', function(){
          $(this).parents('tr').remove();
