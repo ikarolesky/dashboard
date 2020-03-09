d52470734ffa834d69944b5f3bd4b8fa65388aaa
Igor Karolesky <igorfk96@gmail.com>
2020-03-09 17:23:16 -0300

    Script + - Produtos corrigido

--- resources/views/products/_forms2.blade.php
+++ resources/views/products/_forms2.blade.php
@@ -31,6 +31,8 @@
         $("#dynamicTable").append('<tr><td><select id="plataforma" name="addmore['+i+'][plataforma]" class="form-control">@foreach($plataforma as $plat)<option value="{{$plat->id}}">{{$plat->name}}</option>@endforeach</select></td><td><input type="text" name="addmore['+i+'][codigo_produto]" class="form-control" /></td><td><input type="text" name="addmore['+i+'][product_key]" class="form-control" /></td><td><input type="text" name="addmore['+i+'][basic_authentication]" class="form-control" /></td><td><button type="button" class="btn btn-icon btn-icon rounded-circle btn-danger mr-1 mb-1 remove-tr"><i class="feather icon-minus"></i></button></td></tr>');
     });
    $(document).on('click', '.remove-tr', function(){
+    --i;
+    if (i = 0) return;
          $(this).parents('tr').remove();
     });
 </script>
