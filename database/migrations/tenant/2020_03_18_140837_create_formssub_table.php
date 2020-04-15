7342297ba7cc75ab316b0e1615ea03c8b08c7613
Igor Karolesky <igorfk96@gmail.com>
2020-04-15 11:29:53 -0300

    migration alterada

--- database/migrations/tenant/2020_03_18_140837_create_formssub_table.php
+++ database/migrations/tenant/2020_03_18_140837_create_formssub_table.php
@@ -28,10 +28,10 @@ class CreateFormssubTable extends Migration
         Schema::create('forms', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('nome_form');
-            $table->string('conteudo1');
-            $table->string('conteudo2');
-            $table->string('conteudo3');
-            $table->string('conteudo4');
+            $table->text('conteudo1');
+            $table->text('conteudo2');
+            $table->text('conteudo3');
+            $table->text('conteudo4');
             $table->string('url');
             $table->string('produto');
             $table->unsignedInteger('user_id');
